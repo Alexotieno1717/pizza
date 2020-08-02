59663a0a4eca137c5bc3492a53e2550ec1c8411b
Alexotieno1717 <alexotieno900@gmail.com>
2020-08-02 21:16:46 +0300

    Modified js and index file

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// function GetPizza(pizza, size, crust, topping) {
+//   this.pizza = pizza;
+//   this.size = size;
+//   this.crust = crust;
+//   this.topping = topping;
+// }
+
+// GetPizza.prototype.total = function(){
+//     return this.pizza + this.size + this.crust + this.topping;
+// }
+
+// $(document).ready(function () {
+//   $("button#submit").click(function () {
+//     let inputtedPizza = $("#pizza option:selected").val();
+//     let inputtedSize = $("#size option:selected").val();
+//     let inputtedCrust = $("#crust option:selected").val();
+//     let inputtedTopping = $("#topping option:selected").val();
+//     alert(`heyyy`)
+//   });
+  
+// });
